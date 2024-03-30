5b4e957928bd8ba3d06f00c4e61aec8c665d8393
Umairkhan2324 <asadsher23242gmail.com>
2024-03-30 21:17:15 +0500

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,19 +7,21 @@ const Navbar = () => {
     <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
         <div className="relative">
-          { <Image
-            src="/horseLogo.jpg"
-            alt="logo"
-            width={40}
-            height={40}
-            className="w-full h-full object-contain rounded-full"
-          /> }
+          {
+            <Image
+              src="/horseLogo.jpg"
+              alt="logo"
+              width={40}
+              height={40}
+              className="w-full h-full object-contain rounded-full"
+            />
+          }
         </div>
         <h1 className="text-white text-[25px] font-semibold">
-          Webchain{" "}
+          Umair{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
             {" "}
-            Dev{" "}
+            Khan{" "}
           </span>
         </h1>
       </div>
